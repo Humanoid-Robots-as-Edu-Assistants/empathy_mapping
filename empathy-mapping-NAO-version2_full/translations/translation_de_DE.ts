54c08633d235c5bf8d8b9a2924e495c6a046135e
Mitomatos <122729539+Mitomatos@users.noreply.github.com>
2023-09-06 12:23:03 +0200

    Add files via upload
    Version am 06.09.23
    Hinweis: 
    Interaktion beginnt wenn NAO angesprochen bzw. begrüßt wird z.B. "Hallo" ODER
    NAO den Befehl "Spring!" hört (in diesem Fall kann zu einer Frage gesprungen werden)

---
+++ empathy-mapping-NAO-version2_full/translations/translation_de_DE.ts
@@ -0,0 +1,343 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hallo</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Aufgabe zum Einstieg (1)/Say</name>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hallo</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Aufgabe zum Einstieg/Say</name>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hallo</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Danke</name>
+        <message>
+            <source>Danke für eure Teilnahme.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Danke für eure Teilnahme.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/F1 Wer bin ich? (3)/Say</name>
+        <message>
+            <source>Für diese Aufgabe habt ihr 3 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Für diese Aufgabe habt ihr 3 Minuten Zeit.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/F1 Wer bin ich?/Say</name>
+        <message>
+            <source>Für diese Aufgabe habt ihr 3 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Für diese Aufgabe habt ihr 3 Minuten Zeit.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/F10 Was motiviert mich/Say</name>
+        <message>
+            <source>“Was motiviert ich?” Für diese letzte Aufgabe habt ihr 6 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">“Was motiviert ich?” Für diese letzte Aufgabe habt ihr 6 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>Was motiviert ich? Für diese letzte Aufgabe habt ihr 6 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Was motiviert ich? Für diese letzte Aufgabe habt ihr 6 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>Was motiviert mich? Für diese letzte Aufgabe habt ihr 6 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Was motiviert mich? Für diese letzte Aufgabe habt ihr 6 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>Was motiviert mich? Für diese letzte Aufgabe habt ihr noch einmal fünf Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Was motiviert mich? Für diese letzte Aufgabe habt ihr noch einmal fünf Minuten Zeit.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/F2 Wie fuele ich mich?/Say</name>
+        <message>
+            <source>Alles klar?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Alles klar?</translation>
+        </message>
+        <message>
+            <source>Naja, zum Glück kann ich kein Stress empfinden. Ihr seid ja eine ganz schön große Gruppe. Wo waren wir? Ach stimmt, kann es weiter gehen? Drücke bitte kurz einen meiner Fußbumper, wenn wir fortfahren können.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Naja, zum Glück kann ich kein Stress empfinden. Ihr seid ja eine ganz schön große Gruppe. Wo waren wir? Ach stimmt, kann es weiter gehen? Drücke bitte kurz einen meiner Fußbumper, wenn wir fortfahren können.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/F3 Was denke ich? (2)/Say</name>
+        <message>
+            <source>“Was denke ich?” Für diese Aufgabe habt ihr 6 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">“Was denke ich?” Für diese Aufgabe habt ihr 6 Minuten Zeit.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/F3 Was denke ich?/Say</name>
+        <message>
+            <source>Für diese Aufgabe habt ihr 3 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Für diese Aufgabe habt ihr 3 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>“Was denke ich?” Für diese Aufgabe habt ihr 6 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">“Was denke ich?” Für diese Aufgabe habt ihr 6 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>Was denke ich? Für diese Aufgabe habt ihr 6 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Was denke ich? Für diese Aufgabe habt ihr 6 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>Was denke ich? Für diese Aufgabe habt ihr vier Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Was denke ich? Für diese Aufgabe habt ihr vier Minuten Zeit.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/F4 Was wuensche ich mir/Say</name>
+        <message>
+            <source>“Was wünsche ich mir?” Für diese Aufgabe habt ihr 6 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">“Was wünsche ich mir?” Für diese Aufgabe habt ihr 6 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>Was wünsche ich mir? Für diese Aufgabe habt ihr 6 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Was wünsche ich mir? Für diese Aufgabe habt ihr 6 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>Was wünsche ich mir? Für diese Aufgabe habt ihr vier Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Was wünsche ich mir? Für diese Aufgabe habt ihr vier Minuten Zeit.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/F4 Was wuensche ich mir/weiter?</name>
+        <message>
+            <source>Kommt ihr gut vorran? Oder braucht ihr eine Pause? Wenn es weiter gehen kann, drückt einfach einen meiner Fußbumper.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Kommt ihr gut vorran? Oder braucht ihr eine Pause? Wenn es weiter gehen kann, drückt einfach einen meiner Fußbumper.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/F5 Was weiss ich/Say</name>
+        <message>
+            <source>“Was weiss ich?” Für diese Aufgabe habt ihr wieder 6 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">“Was weiss ich?” Für diese Aufgabe habt ihr wieder 6 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>Was weiss ich? Für diese Aufgabe habt ihr wieder 6 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Was weiss ich? Für diese Aufgabe habt ihr wieder 6 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>Was weiss ich? Für diese Aufgabe habt ihr wieder 5 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Was weiss ich? Für diese Aufgabe habt ihr wieder 5 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>Was weiss ich? Für diese Aufgabe habt ihr wieder vier Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Was weiss ich? Für diese Aufgabe habt ihr wieder vier Minuten Zeit.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/F6 Was kann ich/Say</name>
+        <message>
+            <source>“Was kann ich?” Für diese Aufgabe habt ihr 6 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">“Was kann ich?” Für diese Aufgabe habt ihr 6 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>Was kann ich? Für diese Aufgabe habt ihr 6 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Was kann ich? Für diese Aufgabe habt ihr 6 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>Was kann ich? Für diese Aufgabe habt ihr 5 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Was kann ich? Für diese Aufgabe habt ihr 5 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>Was kann ich? Für diese Aufgabe habt ihr vier Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Was kann ich? Für diese Aufgabe habt ihr vier Minuten Zeit.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/F6 Was kann ich/say</name>
+        <message>
+            <source>Sehr gut.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Sehr gut.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/F6 Was kann ich/weiter?</name>
+        <message>
+            <source>Oki doki. Könnt ihr euch eure Persona immer besser vorstellen?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Oki doki. Könnt ihr euch eure Persona immer besser vorstellen?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/F7 Was mach ich/Say</name>
+        <message>
+            <source>“Was mache ich?” Für diese Aufgabe habt ihr 6 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">“Was mache ich?” Für diese Aufgabe habt ihr 6 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>Was mache ich? Für diese Aufgabe habt ihr 6 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Was mache ich? Für diese Aufgabe habt ihr 6 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>Was mache ich? Für diese Aufgabe habt ihr vier Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Was mache ich? Für diese Aufgabe habt ihr vier Minuten Zeit.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/F8 Wie verhalte ich mich/Say</name>
+        <message>
+            <source>“Wie verhalte ich mich?” Für diese Aufgabe habt ihr 6 Minuten Zeit.
+</source>
+            <comment>Text</comment>
+            <translation type="obsolete">“Wie verhalte ich mich?” Für diese Aufgabe habt ihr 6 Minuten Zeit.
+</translation>
+        </message>
+        <message>
+            <source>Wie verhalte ich mich? Für diese Aufgabe habt ihr 6 Minuten Zeit.
+</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Wie verhalte ich mich? Für diese Aufgabe habt ihr 6 Minuten Zeit.
+</translation>
+        </message>
+        <message>
+            <source>Wie verhalte ich mich? Für diese Aufgabe habt ihr vier Minuten Zeit.
+</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Wie verhalte ich mich? Für diese Aufgabe habt ihr vier Minuten Zeit.
+</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/F9 Was demotiviert mich/Say</name>
+        <message>
+            <source>“Was demotiviert ich?” Für diese Aufgabe habt ihr 6 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">“Was demotiviert ich?” Für diese Aufgabe habt ihr 6 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>Also: Was demotiviert ich? Für diese Aufgabe habt ihr 6 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Also: Was demotiviert ich? Für diese Aufgabe habt ihr 6 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>Also: Was demotiviert mich? Für diese Aufgabe habt ihr 6 Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Also: Was demotiviert mich? Für diese Aufgabe habt ihr 6 Minuten Zeit.</translation>
+        </message>
+        <message>
+            <source>Also: Was demotiviert mich? Für diese Aufgabe habt ihr fünf Minuten Zeit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Also: Was demotiviert mich? Für diese Aufgabe habt ihr fünf Minuten Zeit.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hallo</translation>
+        </message>
+        <message>
+            <source>Tschüss und bis zum nächsten Mal!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Tschüss und bis zum nächsten Mal!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hallo</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Uff</name>
+        <message>
+            <source>Uff, ganz schön anstrengend so eine ganze Übung.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Uff, ganz schön anstrengend so eine ganze Übung.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/exercise start/Animated Say</name>
+        <message>
+            <source>Hallo zusammen</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hallo zusammen</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/exercise start/Say</name>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hallo</translation>
+        </message>
+        <message>
+            <source>Danke für die Streicheleinheiten!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Danke für die Streicheleinheiten!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/exercise start/Say (1)</name>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hallo</translation>
+        </message>
+        <message>
+            <source>wenn ein Schritt zu Ende ist.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">wenn ein Schritt zu Ende ist.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/tschuess</name>
+        <message>
+            <source>Tschüss und bis zum nächsten Mal!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Tschüss und bis zum nächsten Mal!</translation>
+        </message>
+    </context>
+</TS>
